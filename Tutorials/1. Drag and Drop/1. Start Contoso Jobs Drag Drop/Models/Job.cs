f5b2c4ef683057fa0e00b2c39e7e423f230c777f
dr.neil <dr.neil@nsquaredsolutions.com>
2016-12-27 13:11:08 +1100

    Add Drag and Drop tutorial

---
+++ Tutorials/1. Drag and Drop/1. Start Contoso Jobs Drag Drop/Models/Job.cs
@@ -0,0 +1,19 @@
+﻿using Contoso_Jobs.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contoso_Jobs.Models
+{
+    public class Job 
+    {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+        
+        public JobStatus Status { get; set; }
+        
+    }
+}
