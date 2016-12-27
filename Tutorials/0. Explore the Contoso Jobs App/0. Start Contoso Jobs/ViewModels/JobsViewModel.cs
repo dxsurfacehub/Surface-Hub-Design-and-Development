29e8dc335bf014d1fba2114ad4b5c49e54854044
dr.neil <dr.neil@nsquaredsolutions.com>
2016-12-27 11:08:21 +1100

    Add the tutorial to explore the Contoso Jobs app

---
+++ Tutorials/0. Explore the Contoso Jobs App/0. Start Contoso Jobs/ViewModels/JobsViewModel.cs
@@ -0,0 +1,129 @@
+﻿using Contoso_Jobs.Common;
+using Contoso_Jobs.Models;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Contoso_Jobs.ViewModels
+{
+    public class JobsViewModel : BindableBase
+    {
+        private ConcurrentBag<Job> jobs;
+        
+        private bool showingMenu;
+
+        public bool ShowingMenu { get { return showingMenu; } set { SetProperty(ref showingMenu, value); } }
+
+        public JobsViewModel()
+        {
+            LoadJobs();
+        }
+
+        public IEnumerable<Job> Backlog
+        {
+            get
+            {
+                if (null == jobs)   
+                {
+                    LoadJobs();
+                }
+                if (null != jobs)
+                {
+                    return jobs.Where<Job>((J, b) => { return J.Status == JobStatus.Backlog; }).ToList();
+                }
+                return jobs;
+            }
+        }
+
+        public IEnumerable<Job> WIP
+        {
+            get
+            {
+                if (null == jobs)
+                {
+                    LoadJobs();
+                }
+                if (null != jobs)
+                {
+                    return jobs.Where<Job>((J, b) => { return J.Status == JobStatus.WIP; }).ToList();
+                }
+                return jobs;
+            }
+        }
+
+        public IEnumerable<Job> Done
+        {
+            get
+            {
+                if (null == jobs)
+                {
+                    LoadJobs();
+                }
+                if (null != jobs)
+                {
+                    return jobs.Where<Job>((J, b) => { return J.Status == JobStatus.Done; }).ToList();
+                }
+                return jobs;
+            }
+        }
+
+        internal void CreateJob(string title, string description)
+        {
+            Job j = new Job() { Title = title, Description = description, Status = JobStatus.Backlog };
+            jobs.Add(j);
+            SaveJobs();
+            OnPropertyChanged("Backlog");
+        }
+
+        internal void EditJob(Job job)
+        {
+            if (job != null)
+            {
+                SaveJobs();
+                if (job.Status == JobStatus.Backlog)
+                {
+                    OnPropertyChanged("Backlog");
+                }
+                else if (job.Status == JobStatus.WIP)
+                {
+                    OnPropertyChanged("WIP");
+                }
+            }
+        }
+        
+        public void StartJob(Job j)
+        {
+            if (j != null
+                && j.Status == JobStatus.Backlog)
+            {
+                j.Status = JobStatus.WIP;
+                OnPropertyChanged("Backlog");
+                OnPropertyChanged("WIP");
+                SaveJobs();
+            }
+        }
+
+        public void CompleteJob(Job j)
+        {
+            if (j != null
+                && j.Status == JobStatus.WIP)
+            {
+                j.Status = JobStatus.Done;
+                OnPropertyChanged("WIP");
+                OnPropertyChanged("Done");
+                SaveJobs();
+            }
+        }
+
+        private async void LoadJobs()
+        {
+            List<Job> js = await XmlService.LoadJobs();
+            jobs = new ConcurrentBag<Job>(js);
+        }
+
+        private async void SaveJobs()
+        { 
+            await XmlService.SaveJobs(jobs.ToList());
+        }
+    }
+}
