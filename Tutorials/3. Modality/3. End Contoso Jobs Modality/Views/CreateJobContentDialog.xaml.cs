b04f4b9c5a470babf518044fc3e6ab59ac7f32bd
Dr. Neil <dr.neil@nsquaredsolutions.com>
2017-01-17 09:47:21 +1100

    Adding code and docs showing how to remove modality from the Contoso Jobs app

---
+++ Tutorials/3. Modality/3. End Contoso Jobs Modality/Views/CreateJobContentDialog.xaml.cs
@@ -0,0 +1,47 @@
+﻿using Contoso_Jobs.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Input.Inking;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Content Dialog item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Contoso_Jobs.Views
+{
+    public sealed partial class CreateJobContentDialog : ContentDialog
+    {
+        private JobsViewModel jobsViewModel;
+        public bool CreateAnother { get; private set; }
+
+        public CreateJobContentDialog(JobsViewModel jobs)
+        {
+            jobsViewModel = jobs;
+            this.InitializeComponent();
+        }
+
+        private void ContentDialog_CreateButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+            IReadOnlyList<InkStroke> strokes = Annotation.InkPresenter.StrokeContainer.GetStrokes();
+            jobsViewModel.CreateJob(titleTextBox.Text, descriptionTextBox.Text,
+                strokes.ToList());
+
+            CreateAnother = createAnotherCheckBox.IsChecked.Value;
+        }
+
+        private void ContentDialog_CancelButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+
+        }
+    }
+}
---
+++ Tutorials/3. Modality/3. End Contoso Jobs Modality/Views/EditJobContentDialog.xaml.cs
@@ -0,0 +1,54 @@
+﻿using Contoso_Jobs.Models;
+using Contoso_Jobs.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Content Dialog item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Contoso_Jobs.Views
+{
+    public sealed partial class EditJobContentDialog : ContentDialog
+    {
+        private JobsViewModel jobsViewModel;
+        private Job job;
+
+        public EditJobContentDialog(Job job, JobsViewModel jobs)
+        {
+            this.job = job;
+            jobsViewModel = jobs;
+            this.InitializeComponent();
+            if (job != null)
+            {
+                titleTextBox.Text = job.Title;
+                descriptionTextBox.Text = job.Description;
+            }
+        }
+
+        private void ContentDialog_SaveButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+            if (null != this.job)
+            {
+                job.Title = titleTextBox.Text;
+                job.Description = descriptionTextBox.Text;
+                jobsViewModel.EditJob(job);
+            }
+            
+        }
+
+        private void ContentDialog_CancelButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+        }
+    }
+}
